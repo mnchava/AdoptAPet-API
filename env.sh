36d6fbe4d2d97a3a15ea8a3a089672122a46f082
Jesus Salvador Uribe <43125746+mnchava@users.noreply.github.com>
2021-09-22 21:08:56 -0500

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='development'
+export PORT=3000
+export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
+export MONGODB_URI='mongodb+srv://adminBEDU:1701@bedu-web.3qryh.mongodb.net/Adoptapet?retryWrites=true&w=majority'
