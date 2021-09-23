34894b716cf3c97e2a2ada8933c4d3872e88870b
mnchava <mnchava@outlook.com>
2021-09-22 21:16:36 -0500

    deploy

--- env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=3000
-export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
-export MONGODB_URI='mongodb+srv://adminBEDU:1701@bedu-web.3qryh.mongodb.net/Adoptapet?retryWrites=true&w=majority'
